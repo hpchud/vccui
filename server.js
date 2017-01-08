cc260d71275bdfc50b35e1b41d184aecf4b7b061
Joshua Higgins <joshiggins@gmail.com>
2017-01-08 02:03:51 +0000

    disconnect socket if process dies

--- server.js
+++ server.js
@@ -254,7 +254,8 @@ var setupSocket = function(socket, data) {
             socket.emit('output', data);
         });
         term.on('exit', function(code) {
-            console.log((new Date()) + " PID=" + term.pid + " ENDED")
+            console.log((new Date()) + " PID=" + term.pid + " ENDED");
+            socket.disconnect(0);
         });
         socket.on('resize', function(data) {
             term.resize(data.col, data.row);
